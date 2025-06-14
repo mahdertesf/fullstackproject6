3fce61cd0bd4fbe1ffe11f1b9a37f91bc4a7bb12
Mahder Tesfaye <mahdertesfayeon@gmail.com>
2025-06-14 21:43:50 +0000

    check again and fix if anything is wring

--- src/app/(main)/admin/audit-log/page.tsx
+++
@@ -1,3 +0,0 @@
-// This file is intentionally left empty as it's being removed.
-// If the build process requires a file to exist, this placeholder prevents an error.
-// The file will be deleted by the file operation mechanism.
